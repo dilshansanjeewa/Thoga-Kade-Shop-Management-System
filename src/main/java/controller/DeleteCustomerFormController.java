694a323e192cc217c1336a86865db64233fa3327
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2025-08-11 19:55:57 +0530

    Create Delete Customer Form

--- src/main/java/controller/DeleteCustomerFormController.java
+++ src/main/java/controller/DeleteCustomerFormController.java
@@ -1,5 +1,59 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class DeleteCustomerFormController {
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Label lblCity;
+
+    @FXML
+    private Label lblDob;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Label lblPostalCode;
+
+    @FXML
+    private Label lblProvince;
+
+    @FXML
+    private Label lblSalary;
+
+    @FXML
+    private Label lblStreetAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
 
 }
