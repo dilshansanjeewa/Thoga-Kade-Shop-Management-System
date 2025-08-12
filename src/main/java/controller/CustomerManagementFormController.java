6feb089c184a042ee963003c8937bdd4bcf9e85f
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2025-08-12 19:22:21 +0530

    add sub customer UIs to main UI

--- src/main/java/controller/CustomerManagementFormController.java
+++ src/main/java/controller/CustomerManagementFormController.java
@@ -2,9 +2,20 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerManagementFormController {
+
+    private Stage addCustomerStage = new Stage();
+    private Stage updataCustomerStage = new Stage();
+    private Stage deleteCustomerStage = new Stage();
+    private Stage viewCustomerStage = new Stage();
 
     @FXML
     private Button btnAddCustomer;
@@ -23,12 +34,22 @@ public class CustomerManagementFormController {
 
     @FXML
     void btnAddCustomerOnAction(ActionEvent event) {
-
+        try {
+            addCustomerStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Add_Customer_Form.fxml"))));
+            addCustomerStage.show();
+        } catch (IOException e) {
+            showAlerts(e.getClass().getName(), e.getMessage());
+        }
     }
 
     @FXML
     void btnDeleteCustomerOnAction(ActionEvent event) {
-
+        try {
+            deleteCustomerStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Delete_Customer_Form.fxml"))));
+            deleteCustomerStage.show();
+        } catch (IOException e) {
+            showAlerts(e.getClass().getName(), e.getMessage());
+        }
     }
 
     @FXML
@@ -38,12 +59,32 @@ public class CustomerManagementFormController {
 
     @FXML
     void btnUpdateCustomerOnAction(ActionEvent event) {
-
+        try {
+            updataCustomerStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Update_Customer_Form.fxml"))));
+            updataCustomerStage.show();
+        } catch (IOException e) {
+            showAlerts(e.getClass().getName(), e.getMessage());
+        }
     }
 
     @FXML
     void btnViewCustomerOnAction(ActionEvent event) {
-
+        try {
+            viewCustomerStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/VIew_Customer_Form.fxml"))));
+            viewCustomerStage.show();
+        } catch (IOException e) {
+            showAlerts(e.getClass().getName(), e.getMessage());
+        }
     }
 
+    private void showAlerts(String title, String content){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Alert");
+        alert.setHeaderText(title);
+        alert.setContentText(content);
+
+        Stage stage = (Stage) btnAddCustomer.getScene().getWindow();
+        alert.initOwner(stage);
+        alert.show();
+    }
 }
--- src/main/java/controller/ThogaKadeHomePageFormController.java
+++ src/main/java/controller/ThogaKadeHomePageFormController.java
@@ -2,9 +2,18 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ThogaKadeHomePageFormController {
+
+    private Stage customerStage = new Stage();
+    private Stage itemStage = new Stage();
 
     @FXML
     private Button btnCustomerManagement;
@@ -14,12 +23,28 @@ public class ThogaKadeHomePageFormController {
 
     @FXML
     void btnCustomerManagementOnaction(ActionEvent event) {
-
+        try {
+            customerStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Customer_Management_Form.fxml"))));
+            customerStage.show();
+        } catch (IOException e) {
+            showAlert(e.getClass().getName(), e.getMessage());
+        }
     }
 
     @FXML
     void btnItemManagementOnAction(ActionEvent event) {
 
+    }
+
+    private void showAlert (String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Alert");
+        alert.setHeaderText(title);
+        alert.setContentText(content);
+
+        Stage stage = (Stage) btnCustomerManagement.getScene().getWindow();
+        alert.initOwner(stage);
+        alert.show();
     }
 
 }
