e6898d3eb8ea2a8ad99076366f98cb4b0f427e07
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2025-08-12 11:04:45 +0530

    create view customer form

---
+++ src/main/java/controller/ViewCustomerFormController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ViewCustomerFormController {
+
+    @FXML
+    private Button btnReload;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode;
+
+    @FXML
+    private TableColumn<?, ?> colProvince;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableColumn<?, ?> colStreetAddress;
+
+    @FXML
+    private TableView<?> tblDetails;
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+}
