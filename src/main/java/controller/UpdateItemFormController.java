0d406722f5b6d6386b004d9cedb2ba3123e29cba
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2025-08-13 13:06:19 +0530

    created Search Item Form and Controller.

---
+++ src/main/java/controller/UpdateItemFormController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UpdateItemFormController {
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private JFXComboBox<?> comboPackSize;
+
+    @FXML
+    private TextField txtCode;
+
+    @FXML
+    private JFXTextField txtDescription;
+
+    @FXML
+    private JFXTextField txtPackSize;
+
+    @FXML
+    private JFXTextField txtQtyonHand;
+
+    @FXML
+    private JFXTextField txtUnitPrice;
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
