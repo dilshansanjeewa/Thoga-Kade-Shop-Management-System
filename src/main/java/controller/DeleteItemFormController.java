cf65f13f82dfbe4fe2bdeae82f1562a26f04d5ae
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2025-08-13 13:24:01 +0530

    created Delete Item Form and Controller.

---
+++ src/main/java/controller/DeleteItemFormController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DeleteItemFormController {
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Label lblDescription;
+
+    @FXML
+    private Label lblPackSize;
+
+    @FXML
+    private Label lblQtyonHand;
+
+    @FXML
+    private Label lblUnitPrice;
+
+    @FXML
+    private TextField txtCode;
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+}
